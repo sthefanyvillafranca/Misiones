fef25d84907ebfaa4abaefb351500c1d87d151fb
sthefanyvillafranca <svillsei@myuax.com>
2026-03-12 08:48:09 +0100

    feat(main): declaracion de variables e inicializacion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        GestionDeMisiones gestor;
+        InterfazConsola interfaz;
+
+        gestor = new GestionDeMisiones();
+
+        interfaz = new InterfazConsola();
+
     }
 
 }
